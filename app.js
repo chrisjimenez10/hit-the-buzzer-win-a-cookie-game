1599c1774e384594bd5b47f2f0477e2303f487e5
chris <jimenezchristopher5@gmail.com>
2024-04-11 03:25:40 -0500

    enhanced current features and added game win message

--- app.js
+++ app.js
@@ -9,44 +9,59 @@ const satisfactionBarDisplay = document.getElementById("satisfaction-bar");
 
 const bottomCell = document.querySelector(".bottom-cell");
 
+const cookieJarIcon = document.getElementById("jar");
+
+const cookieJarDisplay = document.getElementById("cookie-jar-display");
+
 //--------------- Variables ----------------\\
 let cookies = [];
-let satisfactionLevel = ["Very Hungry!", "Hungry", "Small Portion Please!", "I'm Stuffed!"];
-
+let satisfactionLevel = ["Very Hungry!", "Hungry", "Small Portion Please!", "I'm Stuffed!", "I'm at my Limit!"];
 
 //--------------- Functions ---------------\\
 function satisfactionBarMessage(){
     switch(cookies.length){
-        case 2:
+        case 4:
             satisfactionBarDisplay.value = satisfactionLevel[0];
             break;
-        case 4:
+        case 6:
             satisfactionBarDisplay.value = satisfactionLevel[1];
             break;
-        case 6:
+        case 8:
             satisfactionBarDisplay.value = satisfactionLevel[2];
             break;
-        case 8:
+        case 12:
             satisfactionBarDisplay.value = satisfactionLevel[3];
             break;
-        case 9:
+        case 16:
+            satisfactionBarDisplay.value = satisfactionLevel[4];
+            break;
         default:
-            if (cookies.length >= 9){
+            if(cookies.length >= 17){
                 satisfactionBarDisplay.value = "Stop Eating!";
             }else{
-            satisfactionBarDisplay.value = "Keep Eating!";
+                satisfactionBarDisplay.value = "Keep Eating!";
             }
             break;
         }
     }
-    
+ 
 function handleCookieBtnClick(){
     winCookieDisplay.value = "You won a Cookie!";
     cookies.push({food: "snack",type: "cookie",})
-    console.log(cookies);
+    if(cookies.length >= 20){
+        gameWinAnimation();
+    }else{
+    gameWinText.textContent = "";
+    // console.log(cookies);
+    }
 }
+
 function handleSatisfactionBtnClick(){
-    satisfactionBarMessage();    
+    satisfactionBarMessage();
+}
+
+function handleCookieJar(){
+    cookieJarDisplay.value = `You have ${cookies.length} cookies!`;
 }
 
 
@@ -55,16 +70,21 @@ satisfactionBtn.addEventListener("click", handleSatisfactionBtnClick);
 
 winCookieBtn.addEventListener("click", handleCookieBtnClick);
 
-//--------------- Game Winning Condition ---------------\\
-function gameWinCondition(){
-    if(satisfactionBarDisplay.value === "Stop Eating!"){
-        
-    }
+cookieJarIcon.addEventListener("mouseover", handleCookieJar);
+
+cookieJarIcon.addEventListener("mouseout", ()=>{
+    cookieJarDisplay.value = `Cookies Inside Jar!`;
+});
+
+
+//--------------- Game Winning Animation ---------------\\
+function gameWinAnimation(){
+    const gameWinContainer = document.querySelector("#right-text");
+    const gameWinText = document.createElement("h1");
+    gameWinText.textContent = `Congratulations, You Fed your Hunger and Won!`;
+    gameWinContainer.appendChild(gameWinText);
 }
 
-// while(satisfactionBarDisplay.value !== "Stop Eating!"){
-        
-// }
 
 
 
